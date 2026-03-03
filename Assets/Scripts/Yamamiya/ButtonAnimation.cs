bd5b2fbb9c57f0b50e3dc10823b16b7902594c5e
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2026-03-03 14:02:15 +0900

    [add] ボタン選択時のアニメーション ButtonAnimation.csを実装。

---
+++ Assets/Scripts/Yamamiya/ButtonAnimation.cs
@@ -0,0 +1,53 @@
+using LitMotion;
+using LitMotion.Extensions;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    [SerializeField] private RectTransform _rect;
+    [SerializeField] private float _animationDuration = 0.2f;
+    [SerializeField] private Vector3 _defaultScale = Vector3.one;
+    [SerializeField] private Vector3 _selectScale = new Vector3(1.5f, 1.5f, 1.5f);
+    [SerializeField] private Ease _ease = Ease.Linear;
+
+    private MotionHandle _currentMotion;
+
+    private void Awake()
+    {
+        if (_rect == null)
+        {
+            _rect = GetComponent<RectTransform>();
+        }
+
+        _rect.localScale = _defaultScale;
+    }
+
+    private void OnDestroy()
+    {
+        _currentMotion.TryCancel();
+    }
+
+    /// <summary>
+    /// ボタンが選択されたときのアニメーションを行う。
+    /// </summary>
+    public void OnSelect()
+    {
+        _currentMotion.TryCancel();
+
+        _currentMotion = LMotion.Create(_rect.localScale, _selectScale, _animationDuration)
+            .WithEase(_ease)
+            .BindToLocalScale(_rect);
+    }
+
+    /// <summary>
+    /// ボタンが選択解除されたときのアニメーションを行う。
+    /// </summary>
+    public void OnDeselect()
+    {
+        _currentMotion.TryCancel();
+
+        _currentMotion = LMotion.Create(_rect.localScale, _defaultScale, _animationDuration)
+            .WithEase(_ease)
+            .BindToLocalScale(_rect);
+    }
+}
