3b6571525fa44191dabc978dcfd662ac19fa0b6b
ChouTakushin <takushin.chou@gmail.com>
2026-03-04 02:49:03 +0900

    [add/update] 連打機能実装 | キーボード叩き機能実装 | 連打システムPrefab化 | Tester作成

---
+++ Assets/Scripts/Chou/Enums/EnumTurn.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの押付／相手の反論を区別するEnum
+/// </summary>
+public enum EnumTurn
+{
+    Player,
+    Opponent
+}
---
+++ Assets/Scripts/Chou/KeyboardSmashController.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KeyboardSmashController : MonoBehaviour
+{
+    [SerializeField, Range(0f, 0.1f), Tooltip("キーボード叩きの受付時間（秒）")]
+    private float _smashWindow = 0.05f;
+    [SerializeField, Tooltip("スコア倍率最大上昇値")]
+    private float _scoreScaleMax = 0.5f;
+    [SerializeField, Tooltip("最大倍率に必要な押下数")]
+    private int _smashCountForMaxScale = 30;
+
+    private SmashKeyboardActions _smashInput;
+    private bool _smashFlg = false;
+    private int _smashCount = 0;
+    private float _smashScoreRate;
+
+    /// <summary>キーボード叩きが発生した瞬間のデリゲート</summary>
+    public event Action OnSmashed;
+    /// <summary>キーボード叩きが受付終了後のデリゲート</summary>
+    public event Action OnSmashFinished;
+
+    /// <summary>キーボード叩きによるスコア倍率</summary>
+    public float SmashScoreRate => _smashScoreRate;
+
+    #region ライフサイクル
+    private void Awake()
+    {
+        _smashInput = new SmashKeyboardActions();
+        _smashInput.SmashMap.Smash.performed += KeySmashed;
+    }
+    private void OnDisable()
+    {
+        _smashInput.SmashMap.Smash.performed -= KeySmashed;
+    }
+    private void OnDestroy()
+    {
+        _smashInput.SmashMap.Smash.performed -= KeySmashed;
+    }
+    #endregion
+    public void Init()
+    {
+        _smashFlg = false;
+        _smashCount = 0;
+        _smashInput.Enable();
+    }
+
+    private void KeySmashed(InputAction.CallbackContext ctx)
+    {
+        if (!_smashFlg)
+        {
+            _smashFlg = true;
+            // 一定時間内にキーボードの押下を受け付ける
+            Invoke(nameof(CloseSmashWindow), _smashWindow);
+        }
+        _smashCount++;
+    }
+
+    #region Privateメソッド
+    /// <summary>
+    /// キーボード叩きによるスコア倍率を算出する。
+    /// </summary>
+    private void CalcSmashScoreRate()
+    {
+        int keyCount = _smashCount > _smashCountForMaxScale ? _smashCountForMaxScale : _smashCount;
+        _smashScoreRate = 1 + _scoreScaleMax * keyCount / _smashCountForMaxScale;
+    }
+    /// <summary>
+    /// キーボード叩きの受付を停止する
+    /// </summary>
+    private void CloseSmashWindow()
+    {
+        _smashInput.Disable();
+        CalcSmashScoreRate();
+        OnSmashed?.Invoke();
+        OnSmashFinished?.Invoke();
+    }
+    #endregion
+}
