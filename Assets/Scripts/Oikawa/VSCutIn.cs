ee32cedca261d0754ca16a148440c311514638f9
Oikawa <yucchi0424@gmail.com>
2026-03-05 00:16:17 +0900

    [add] 「画面の左右の鉄門のようなパネル」と「キャラクター登場」の演出の実装とサンプルPrefabの追加

---
+++ Assets/Scripts/Oikawa/VSCutIn.cs
@@ -0,0 +1,103 @@
+using Cysharp.Threading.Tasks;
+using LitMotion;
+using LitMotion.Extensions;
+using UnityEngine;
+
+public sealed class VSCutIn : MonoBehaviour
+{
+    [Tooltip("画面全体揺らすためのTransform")]
+    [SerializeField] private Transform _shakeParentT;
+
+    [Tooltip("中心線を出すためのSpriteRenderer")]
+    [SerializeField] private SpriteRenderer _lighting;
+
+    [Tooltip("中心線を出す際のエフェクト用のSpriteRenderer")]
+    [SerializeField] private SpriteRenderer _whiteout;
+
+    [Space]
+
+    [Tooltip("背景の左側のTransform")]
+    [SerializeField] private Transform _leftBackground;
+    [Tooltip("背景の右側のTransform")]
+    [SerializeField] private Transform _rightBackground;
+
+    [Space]
+
+    [SerializeField] private Transform _leftCharactor;
+    [SerializeField] private Transform _rightCharactor;
+
+    [Space]
+
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip _slideClip;
+    [SerializeField] private AudioClip _ironSmashClip;
+
+    public async UniTask PlayAnimationAsync()
+        => PlayAnimation(false);
+    public void PlayAnimation()
+        => PlayAnimation(false).Forget();
+
+    [ContextMenu("PlayTest")]
+    private void PlayTest()
+        => PlayAnimation(true).Forget();
+
+    [ContextMenu("Init")]
+    void Awake()
+    {
+        _shakeParentT.gameObject.SetActive(false);
+    }
+    private async UniTask PlayAnimation(bool isTest = false)
+    {
+        if (isTest)
+        {
+            _shakeParentT.gameObject.SetActive(false);
+            await UniTask.Delay(500);
+        }
+
+
+        _shakeParentT.gameObject.SetActive(true);
+        _whiteout.enabled = false;
+        _lighting.enabled = false;
+        _leftCharactor.gameObject.SetActive(false);
+        _rightCharactor.gameObject.SetActive(false);
+
+
+        //_source.PlayOneShot(_slideClip);
+        var handle = LSequence.Create()
+            .Append(LMotion.Create(-17f, -7f, 0.3f)
+                .WithEase(Ease.InCirc)
+                .BindToLocalPositionX(_leftBackground))
+            .Join(LMotion.Create(17f, 7f, 0.3f)
+                .WithEase(Ease.InCirc)
+                .BindToLocalPositionX(_rightBackground))
+            .Run();
+        handle.Time = 0;
+        await handle;
+        _source.Stop();
+
+        _source.PlayOneShot(_ironSmashClip);
+        _whiteout.enabled = false;
+        _lighting.enabled = true;
+
+        _leftCharactor.gameObject.SetActive(true);
+        _rightCharactor.gameObject.SetActive(true);
+        handle = LSequence.Create()
+             .Join(LMotion.Punch.Create(0f, 0.5f, 1f)
+                .WithEase(Ease.OutQuart)
+                .WithFrequency(Random.Range(5, 10))
+                .BindToLocalPositionX(_shakeParentT))
+            .Join(LMotion.Punch.Create(0f, 0.5f, 1f)
+                .WithEase(Ease.OutQuart)
+                .WithFrequency(Random.Range(5, 10))
+                .BindToLocalPositionY(_shakeParentT))
+
+            .Join(LMotion.Create(-14f, -6f, 0.5f)
+                .WithEase(Ease.OutCirc)
+                .BindToLocalPositionX(_leftCharactor))
+            .Join(LMotion.Create(14f, 6f, 0.5f)
+                .WithEase(Ease.OutCirc)
+                .BindToLocalPositionX(_rightCharactor))
+            .Run();
+        await handle;
+    }
+}
