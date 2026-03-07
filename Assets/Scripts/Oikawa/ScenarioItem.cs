a599abd7cfc01b87d3f9e20f205d7cdd3eefa53a
Oikawa <yucchi0424@gmail.com>
2026-03-07 13:36:27 +0900

    [update] OnDestroy()時にシナリオマネージャーの登録を解除するよう実装。

--- Assets/Scripts/Oikawa/ScenarioItem.cs
+++ Assets/Scripts/Oikawa/ScenarioItem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ScenarioItem : MonoBehaviour
@@ -5,13 +6,31 @@ public class ScenarioItem : MonoBehaviour
     [SerializeField] private SimpleScenarioManager _scenarioManager;
     [SerializeField] private ActionType _actionType;
 
+    private Action<ActionType> _onActionTriggered;
+
     private void Awake()
     {
-        _scenarioManager.OnActionTriggered += type =>
+        if (_scenarioManager == null)
+        {
+            enabled = false;
+            Debug.LogError($"{nameof(ScenarioItem)} の {nameof(_scenarioManager)} が未設定です", this);
+            return;
+        }
+
+        _onActionTriggered = type =>
         {
             if (type == _actionType)
                 gameObject.SetActive(true);
         };
+
+        _scenarioManager.OnActionTriggered += _onActionTriggered;
         gameObject.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        if (_scenarioManager == null || _onActionTriggered is null)
+            return;
+        _scenarioManager.OnActionTriggered -= _onActionTriggered;
+        _onActionTriggered = null;
     }
 }
